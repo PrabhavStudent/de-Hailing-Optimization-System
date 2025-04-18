285f5539a5e9a2e9c553d14996138f9b99a2f52e
Prabhav Singh <prabhavnirwal01@gmail.com>
2025-04-18 17:07:44 +0000

    Install Express and node js

--- server.js
+++ server.js
@@ -5,5 +5,8 @@ const rideRoutes = require('./routes/ride');
 app.use(express.json());
 app.use('/api/ride', rideRoutes);
 
+app.get('/', (req, res) => {
+    res.send('Ride Matching Server is Running!');
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
